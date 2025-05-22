7a9bccb08941b4961a5c7f72cd40e3cf16546f2a
Darwin <darwin.a1120@gmail.com>
2025-05-21 21:46:41 -0500

    Se activa envío de métricas a grafana

--- Backend/src/main/java/grupo6/backendHotel/security/SecurityConfig.java
+++ Backend/src/main/java/grupo6/backendHotel/security/SecurityConfig.java
@@ -87,7 +87,9 @@ public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
                 .and()
                 .authorizeRequests()
                 // Permite acceso libre a la consola H2
-                .antMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/v3/api-docs.yaml", "/debug", "/actuator/**").permitAll()
+                .antMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
+                        "/swagger-resources/**", "/webjars/**", "/configuration/**",
+                        "/swagger-ui/index.html", "/api-docs/**", "/debug", "/actuator/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
