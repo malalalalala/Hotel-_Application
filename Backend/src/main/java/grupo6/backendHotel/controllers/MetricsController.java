88f0d2b9ba645f65b6b13db313855af2c755274a
Darwin <darwin.a1120@gmail.com>
2025-05-29 10:50:18 -0500

    Se agregan metodos controller metrics

--- Backend/src/main/java/grupo6/backendHotel/controllers/MetricsController.java
+++ Backend/src/main/java/grupo6/backendHotel/controllers/MetricsController.java
@@ -1,5 +1,7 @@
 package grupo6.backendHotel.controllers;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,8 @@ import java.lang.management.ManagementFactory;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/metrics")
@@ -27,5 +31,15 @@ public class MetricsController {
         info.put("version", "1.0.0");
         info.put("uptime", ManagementFactory.getRuntimeMXBean().getUptime());
         return info;
+    }
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    @GetMapping("/available")
+    public Set<String> availableMetrics() {
+        return meterRegistry.getMeters()
+                .stream()
+                .map(meter -> meter.getId().getName())
+                .collect(Collectors.toSet());
     }
 }
