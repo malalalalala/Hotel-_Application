470626eeca4f9443c9ba848ea4cd9dfc2ff96a2f
Darwin <darwin.a1120@gmail.com>
2025-05-29 08:53:58 -0500

    Se agrega controller para metricas

--- Backend/src/main/java/grupo6/backendHotel/config/MetricsConfig.java
+++ Backend/src/main/java/grupo6/backendHotel/config/MetricsConfig.java
@@ -3,7 +3,10 @@ package grupo6.backendHotel.config;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class MetricsConfig {
     @Bean
     public PrometheusMeterRegistry prometheusMeterRegistry() {
