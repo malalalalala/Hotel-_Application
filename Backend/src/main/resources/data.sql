1741eaa5abbc5ce33f372de9a2f6a6d87f34236b
Darwin <darwin.a1120@gmail.com>
2025-06-04 14:07:52 -0500

    Se cambia nombre Icono lavanderia

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -743,7 +743,7 @@ INSERT INTO features (name, icon)
   VALUES ('Apto mascotas', 'faPaw');
 
 INSERT INTO features (name, icon)
-  VALUES ('Lavadero', 'faShirt');
+  VALUES ('Lavadero', 'faTshirt');
 
 INSERT INTO features (name, icon)
   VALUES ('Piscina', 'faPersonSwimming');
