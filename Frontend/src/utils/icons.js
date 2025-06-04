5acbf92d9a9e4a73145fc5f18bd74a8c46ab4b13
Darwin <darwin.a1120@gmail.com>
2025-06-04 12:29:06 -0500

    Adición iconos

---
+++ Frontend/src/utils/icons.js
@@ -0,0 +1,21 @@
+import {
+  faWifi,
+  faSnowflake,
+  faKitchenSet,
+  faTV,
+  faPaw,
+  faShirt,
+  faPersonSwimming,
+  faCar
+} from "@fortawesome/free-solid-svg-icons";
+
+export const iconMap = {
+  faWifi: faWifi,
+  faSnowflake: faSnowflake,
+  faKitchenSet: faKitchenSet,
+  faTV: faTV,
+  faPaw: faPaw,
+  faShirt: faShirt,
+  faPersonSwimming: faPersonSwimming,
+  faCar: faCar
+};
